a3199182c642a4329f67045554b4b7ac31ab43fb
AegisSSC <56277974+AegisSSC@users.noreply.github.com>
2022-01-26 16:28:14 -0500

    Attempted to fix matplotlib

--- Predictor.py
+++ Predictor.py
@@ -8,15 +8,13 @@
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 from sklearn.svm import SVR
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 
 #Description: Read in the data from the given csv file so that it can be used by the program
-#Input: filename: is the filepath from the environment to the datafile. 
-#Returns: returns a DataFrame type named datafile so that we can compute information from it
 def read_in_data(filename):
     datafile = pd.read_csv(filename)
 
@@ -34,12 +32,6 @@ def read_in_data(filename):
     return datafile
 
 #Description: Read in the data from the given csv file so that it can be used by the program
-#Input: datafile: the DataFrame containing the csv file information
-#       predictionDays, the total number of days that you are wanting to predict
-#       independent_array, the 'x' value array that will be used to predict the price of bitcoin
-#       dependent_array, the 'y' value array that holds the corresponding prices from the predictor
-#       test_percent: the value that is a percentage (between 0 and 1) that represents the portion of 'x' that will be reserved for testing
-#Returns: returns a DataFrame type named datafile so that we can compute information from it
 def split_for_training(datafile, predictionDays,independent_array, dependent_array, test_percent):
     xtrain, xtest, ytrain, ytest = train_test_split(independent_array, dependent_array, test_size = test_percent)
     predictionDays_array = np.array(datafile.drop(['Prediction'],1))[-predictionDays:]
@@ -47,11 +39,6 @@ def split_for_training(datafile, predictionDays,independent_array, dependent_arr
     return xtrain, xtest, ytrain, ytest, predictionDays_array
 
 #Description: Builds a Support Vector Regression Model for predicting the price of bitcoin
-#Input: xtrain: array containing all of the 'x' values that are for training
-#       xtest: array containing all of the 'x' values that are for testing
-#       ytrain: array containing all of the 'y' values corresponding to 'x' for training
-#       ytest: array containing all of the 'y' values corresponding to 'x' for testing
-#Returns: returns a Support Vector Regression Model 
 def create_svm(xtrain, xtest, ytrain, ytest):
     #create the Support Vector Regression
     svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.00001)
@@ -70,10 +57,9 @@ def create_poly(xtrain, xtest, ytrain, ytest, i):
     poly_reg_confidence = np.poly1d
 
     return poly_reg
+
+
 #Description: Builds and predicts the bitcoin prices for a given number 'n' days
-#Input: datafile: The file that contains all of the values from the csv file
-#       predictionEntries: The number of entries that you want to predict for
-#Returns: returns a Support Vector Regression Model 
 def predict_for_n_entries(datafile, predictionEntries):
     datafile['Prediction'] = datafile[['Price']].shift(-predictionEntries)
     #show the first and last 5 Rows
@@ -82,7 +68,6 @@ def predict_for_n_entries(datafile, predictionEntries):
 
     #Include dynamic file length operation
     total_entries = len(datafile)
-    
     
     # plt.plot(total_entries, datafile['Price'])
     #create the independent data set
@@ -110,7 +95,7 @@ def predict_for_n_entries(datafile, predictionEntries):
     #Plot the Model Predictions for the next 'n' days against the total graph
     
     svm_prediction = svr_rbf.predict(predictionEntries_array)
-    # plt.plot(xtest,svm_prediction, label = "Predicted Outcome")
+    plt.plot(xtest,svm_prediction, label = "Predicted Outcome")
     plt.plot(range(total_entries - predictionEntries, total_entries), svm_prediction, label = "Predicted Outcome")
     plt.plot(range(total_entries - predictionEntries,total_entries), datafile.tail(predictionEntries), label = "Actual Outcome")
     plt.xlabel('x - time interval (days) ')
@@ -121,10 +106,10 @@ def predict_for_n_entries(datafile, predictionEntries):
 
 
 
-    #create a polynomial regression model
-    #find what degree polynomial works best for the model
-    for i in range(0,100):
-        p = np.polyfit(xtrain,ytrain, i)
+    # #create a polynomial regression model
+    # #find what degree polynomial works best for the model
+    # for i in range(0,100):
+    #     p = np.polyfit(xtrain,ytrain, i)
 
 
 def main():
